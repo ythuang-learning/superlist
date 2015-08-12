911bd4ce771c8b367c6ba8e6ca720cde0ea28241
Huang Yung-Tai <ythuang@gmail.com>
2015-08-13 02:08:26 +0800

    add test case description

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,35 @@
-# First functional test
+# Todo List App functional test
+
 from selenium import webdriver
 
 browser = webdriver.Chrome()
+
+# Edith has heard about a cool new online to-do app. She goes
+# to check out it's homepage
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# she notices the page title and header mention to-do lists
+assert 'To-Do' in browser.title
+
+# She is invited to enter a to-do item straight away
+
+# She types "Buy peacock feathers" into a text box (Edith's hobby
+# is tying fly-fishing lures)
+
+# When she hits enter, the pages updates, and now the page lists
+# "1: Buy peacock feathers" as an item in a to-do list
+
+# There is still a text box inviting her to add another item. She
+# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+# The page updates again, and now shows both items on her list
+
+# Edith wonders whether the site will remember her list. Then she sees
+# that the site has generated a unique URL for her -- there is same
+# explanatory text to that effect.
+
+# She visits that URL - her to-do list is still there.
+
+# Satisfied, she goes back to sleep
+
+browser.quit()
