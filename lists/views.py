a86b2c9c11706d1a237211b4552bb4e9f326f3d8
Huang Yung-Tai <ythuang@gmail.com>
2015-08-13 23:53:43 +0800

    add items to existing list, ft pass!

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from lists.models import Item,List
+from lists.models import Item, List
 
 
 # Create your views here.
@@ -7,12 +7,19 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
+    return render(request, 'list.html', {'list': list_})
 
 
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'],list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/%d/' % (list_.id))
+
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/%d/' % (list_.id))
