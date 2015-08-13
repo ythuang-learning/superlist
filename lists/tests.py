990cdb5332c6d64be0dbc7de8d43d862d26235d9
Huang Yung-Tai <ythuang@gmail.com>
2015-08-13 19:15:21 +0800

    create view_list view

--- lists/tests.py
+++ lists/tests.py
@@ -58,7 +58,6 @@ class HomePageTest(TestCase):
         self.assertIn('itemey 2', response.content.decode())
 
 
-
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
         first_item = Item()
@@ -76,3 +75,14 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, first_item_text)
         self.assertEqual(second_saved_item.text, second_item_text)
+
+
+class ListViewTest(TestCase):
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
